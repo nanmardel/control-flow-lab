e5bf241502938a53d0144901fb1a940a28761af3
nanmardel <martin_n1@denison.edu>
2022-04-19 19:16:38 -0500

    lab update

--- exersice-1.py
+++ exersice-1.py
@@ -7,29 +7,15 @@
 letter = input('Enter a letter from "a-z:')
 
 # 2. Write the code that determines whether the letter entered is a vowel
-if letter == 'a':
-    print('You entered a vowel')
-elif letter == 'e':
-    print('You entered a vowel')
-elif letter == 'i':
-    print('You entered a vowel')
-elif letter == 'o':
-    print('You entered a vowel') 
-elif letter == 'u':
-    print('You entered a vowel')    
+if letter == 'aeiou':
+    print(f'The letter "{letter}" is vowel')
 else:
-    print('not a vowel')    
+    print(f'The letter {letter} is a constant')    
 
 # 3. Print one of following messages (substituting the letter for x):
 #      - The letter x is a vowel
 #      - The letter x is a consonant
-letter = 'A'
 
-print(f'The letter "{letter}" is a vowel')
-
-second_letter = 'B'
-
-print(f'The letter "{second_letter}" is a constant')
 
 # Hints:  Use the in operator to check if a character is in another string
 #         For example, if some_char in 'abc':
--- exersice-2.py
+++ exersice-2.py
@@ -6,3 +6,10 @@
 # 2. Print the following message:
 #      - What you entered is xx characters long
 # 3. Return to step 1, unless the word 'quit' was entered.
+
+phrase = ''
+
+while phrase != 'quit':
+    phrase = input('Please enter a word or phrase:').lower()
+    print('What you enteres is ', len(phrase), 'characters long')
+
